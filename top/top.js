c8c3cd1dd1faa9bfc76c22f05f206ce1685506bf
Aimé Calvin RANDRIANANTENAINA <randricalvin@gmail.com>
2022-03-21 16:26:26 +0100

    carousel mobile

---
+++ top/top.js
@@ -0,0 +1,42 @@
+document.body.onload = function () {
+    nbr = 5;
+    p = 0;
+    carousel = document.getElementById("carousel");
+    g = document.getElementById("g");
+    d = document.getElementById("d");
+    carousel.style.width = (100 * nbr) + "vw";
+    for (i = 1; i <= nbr; i++) {
+        div = document.createElement("div");
+        div.className = "image-carousel";
+        div.style.backgroundImage = "url('images/im" + i + ".jpg')";
+        carousel.appendChild(div);
+    }
+    displayHidden();
+
+}
+g.onclick = function () {
+    if (p > -nbr + 3)
+        p--;
+    carousel.style.transform = "translate(" + p * 100 + "vw)";
+    carousel.style.transition = "all 0.5s ease";
+    displayHidden();
+}
+
+d.onclick = function () {
+    if (p < 2)
+        p++;
+    carousel.style.transform = "translate(" + p * 100 + "vw)";
+    carousel.style.transition = "all 0.5s ease";
+    displayHidden();
+}
+
+function displayHidden() {
+    if (p == -nbr + 3)
+        g.style.visibility = "hidden";
+    else
+        g.style.visibility = "visible";
+    if (p == 2)
+        d.style.visibility = "hidden";
+    else
+        d.style.visibility = "visible";
+}
