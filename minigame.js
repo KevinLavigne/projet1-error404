a3fcfd40977131f5d960d428f002cd05356b2f06
KevinLavigne <lavigne.kevin02@gmail.com>
2022-03-22 23:46:44 +0100

    systeme de combat en cour function d'amelioration done dysplay des stats  done

--- minigame.js
+++ minigame.js
@@ -1,6 +1,11 @@
 function RedirectionJavascript(){
     document.location.href="acceuil.html";
 }
+const option1 = document.getElementById('option1')
+const option2 = document.getElementById('option2')
+const option3 = document.getElementById('option3')
+
+const gameText1 = document.getElementById('game-text-1')
 
 const playerMaxHp = document.getElementById('max-health')
 
@@ -26,9 +31,45 @@ const arm = document.getElementById("armory")
 
 const def = document.getElementById('defence')
 
+const damageEnnemy = document.getElementById('damageEnnemy')
+
 let weapon = 1
 
 let armory = 1
+
+let flag = true
+
+const reset = function(){
+    option1.style.display= "none"
+    option2.style.display= "none"
+    option3.style.display= "none"
+
+}
+const armorUpgrade = function(){
+    armory = armory + 1;
+}
+
+const weaponupgrade = function(){
+    weapon = weapon + 1;
+}
+const heal = function(){
+    player[5] = 50
+}
+const endOfFight = function(){
+    gameText1.classList.remove("animated-text")
+    void gameText1.offsetWidth;
+    gameText1.innerHTML = "select your reward"
+    gameText1.classList.add("animated-text")
+    option1.setAttribute("value", "sword updrade")
+    option1.addEventListener("click",() => weaponupgrade())
+    option1.addEventListener("click",() => reset())
+    option2.setAttribute("value", "Armor updrade")
+    option2.addEventListener('click',() => armorUpgrade())
+    option2.addEventListener("click",() => reset())
+    option3.setAttribute("value", "Heal")
+    option3.addEventListener('click',() => heal())
+    option3.addEventListener("click",() => reset())
+}
 
 const player = [
     name = "Momonga",
@@ -37,7 +78,7 @@ const player = [
     vitality = 10,
     MaxHp = (vitality * 5),
     hp = MaxHp * 1,
-    armor = (5-4) * armory,
+    armor = (5 * armory) - 4,
     Dexterity = 10,
     Charisma = 5,
     Luck  = 0,
@@ -65,7 +106,7 @@ class Character {
         str.innerHTML = player[1]
         vita.innerHTML = player[3]
         ennemyName.innerHTML = this.name
-        damage.innerHTML = this.damage 
+        damageEnnemy.innerHTML = this.damage 
         enemyLife.innerHTML = this.life
         ennemyProt.innerHTML= this.damageReduc
         playerMaxHp.innerHTML= player[4]
@@ -73,43 +114,56 @@ class Character {
         weap.innerHTML= weapon
         arm.innerHTML= armory
         def.innerHTML= player[6]
-
+        damage.innerHTML= (player[2] * weapon)
         }
 
      fight(ennemy){
-         this.life = this.life - (player[2] - this.damageReduc);
-         player[5] = player[5] - (this.damage - player[6]);
-         this.render()
-     }
-
-     brawl(){
          if (player[5] > 0 && this.life > 0){
+            this.life = this.life - (player[2] - this.damageReduc);
+            player[5] = player[5] - (this.damage - player[6]);
             this.render()
          }
          if (player[5] <= 0){
-             this.render()
-             alert("you lose try again an another time")
-         }
-         if (this.life <= 0) {
             this.render()
+            alert("you lose try again an another time")
+            RedirectionJavascript()
+         }
+            if (this.life <= 0 && this.name === "wolf") {
+               endOfFight()
+               skeleton.render()
+         }
+         if (this.life <= 0 && this.name === "Skeleton") {
+            endOfFight()
+            archSpider.render()
+         }
+         if (this.life <= 0 && this.name === "ArchSpider") {
+            endOfFight()
+            skeleton.render()
+         }
+         if (this.life <= 0 && this.name === "Werewolf") {
+            endOfFight()
+            skeleton.render()
+         }
+         if (this.life <= 0 && this.name === "Demon") {
+            endOfFight()
+            skeleton.render() 
          }
      }
-
-
 }
+
+option2.addEventListener('click',() => wolf.fight() )
+option2.setAttribute("value", "Smash Him!")
+
 const wolf = new Character ("wolf", 30, 5, 0, true, false);
 wolf.render()
+const skeleton = new Character ("Skeleton", 45, 8, 5, true, false)
 
-
-const skeleton = new Character ("Skeleton", 40, 5, 5, true, false)
-console.log(skeleton)
-
-
+const archSpider = new Character ("Arch Spider", 90, 15, 0,true, false)
 
 
 
 /*
-const archSpider = new Character ("Arch Spider", 25, 10, 0,true, false)
+
 const Werwolf = new Character("Werewolf", 50, 10 , 0,true,true)
 const finalBoss = new Character ("The Redeirection Button", 800, 50, 20, true,false )   
 
