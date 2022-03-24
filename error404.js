df9d8d1e017f0b9969f87b0f02cb4c386ac88e8f
Terry Grimoire <terrt.grimoire@gmail.com>
2022-03-24 23:45:36 +0100

    Adding JS error404 file

--- error404.js
+++ error404.js
@@ -1,6 +1,10 @@
-
-
-
-
-
-
+const button = document.getElementById('bouton-retour')
+const text = document.getElementById('text-event')
+function redirection(){
+    document.location.href="accueil.html";
+}
+function reload(){
+text.style.color ='black'
+ setInterval(redirection, 10000)
+}
+button.addEventListener("click",reload)
