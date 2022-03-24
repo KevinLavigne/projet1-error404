e9a48e1610a88fe7a26d329309ffbdbd3fe6739a
KevinLavigne <lavigne.kevin02@gmail.com>
2022-03-25 00:06:33 +0100

    correction texte minigame ajour d'une phrase de remerciment pour don

--- popup.js
+++ popup.js
@@ -1,4 +1,10 @@
 function myFunction() {
-    var popup = document.getElementById("myPopup");
+    const popup = document.getElementById("myPopup");
     popup.classList.toggle("show");
-  }
+}
+const button = document.getElementById("buttonDonate")
+function thanksYou(){
+  const text = document.getElementById("thank-you");
+  text.innerHTML = "Merci pour votre générosité"
+}
+button.addEventListener("click",thanksYou)
