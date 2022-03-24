5754992048f3f869d598adcfb8b9a1677bab3573
KevinLavigne <lavigne.kevin02@gmail.com>
2022-03-24 10:24:37 +0100

    calul auto reduction de don fait

---
+++ script.js
@@ -0,0 +1,15 @@
+/*
+function getVal() {
+    
+    
+    return reduction
+  }
+window.addEventListener("keypress",getVal)*/
+
+
+function updateReduction(event){
+    const reduction = document.getElementById('reduction')
+    reduction.innerHTML = event.target.value *66 / 100
+}
+const val = document.getElementById('don');
+val.addEventListener("input",(event)=> updateReduction(event))
